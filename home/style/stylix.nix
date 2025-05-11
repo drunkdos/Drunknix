1196ff05d363974fe48aa8dc559050eab0b23aa5
drunkdos <drunkdos.97@gmail.com>
2025-05-11 22:11:45 +0200

    rofi theme

--- home/style/stylix.nix
+++ home/style/stylix.nix
@@ -7,7 +7,7 @@ let
   backgroundSha256 = builtins.readFile (./. + "../../../themes/"+("/"+userSettings.theme)+"/backgroundsha256.txt");
 in
 {
-
+  imports = [ ./rofi.nix];
   home.file.".currenttheme".text = userSettings.theme;
   stylix.enable = true;
   stylix.autoEnable = true;
