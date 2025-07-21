2f9ce74ec7abd0b2e1d28d67ebc3ebfa6b8e7c74
drunkdos <drunkdos.97@gmail.com>
2025-07-22 00:23:33 +0200

    fixed path

--- modules/nh.nix
+++ modules/nh.nix
@@ -1,15 +1,12 @@
-{
-  pkgs,
-  username,
-  ...
-}: {
+{inputs, config, userSettings, lib, pkgs, pkgs-unstable, ... }:
+ {
   programs.nh = {
     enable = true;
     clean = {
       enable = true;
       extraArgs = "--keep-since 7d --keep 5";
     };
-    flake = "/home/${username}/nixos";
+    flake = "/home/${userSettings.username}/nixos";
   };
 
   environment.systemPackages = with pkgs; [
