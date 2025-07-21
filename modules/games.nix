b80035eafaf922c206a67d145e8c7f8bfea38261
drunkdos <drunkdos.97@gmail.com>
2025-07-22 00:24:29 +0200

    gamescope + proton

--- modules/games.nix
+++ modules/games.nix
@@ -7,6 +7,17 @@
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     gamescopeSession.enable = true;
+    extraCompatPackages = [
+      pkgs.proton-ge-bin
+      ];
+    };
+
+    programs.gamescope = {
+      enable = true;
+      capSysNice = true;
+      args = [
+        "--expose-wayland"
+      ];
     };
 
 
@@ -27,7 +38,7 @@
     r2modman
     heroic
     prismlauncher
-
+    sgdboop
       ]);
 
 
