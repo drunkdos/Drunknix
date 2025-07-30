c0ed0a3eea511b3149e7e8918e247846f60bcb5a
drunkdos <drunkdos.97@gmail.com>
2025-07-30 22:36:29 +0200

    new

---
+++ host/DrunkNextop/home.nix
@@ -0,0 +1,29 @@
+{inputs, config, home-manager,stylix, pkgs, lib, ... }:
+
+{
+  home.username = "drunk";
+  home.homeDirectory = "/home/drunk";
+  imports = [
+   ../../home/style/stylix.nix
+   ../../home/hyprland.nix
+   ../../home/app/kitty.nix
+
+  ];
+
+  home.stateVersion = "24.05"; # Do not change.
+
+  home.packages = with pkgs; [
+  ]; 
+
+  home.sessionVariables = { };
+
+  programs.git = {
+    enable = true;
+    userName  = "drunkdos";
+    userEmail = "drunkdos.97@gmail.com";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
+
