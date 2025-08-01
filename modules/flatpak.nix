3d422aee993e0d5eed96197bbad8c9715cee6ea6
drunkdos <drunkdos.97@gmail.com>
2025-08-01 23:07:34 +0200

    Obs Virtual Cam

--- modules/flatpak.nix
+++ modules/flatpak.nix
@@ -1,6 +1,16 @@
 {config, lib, pkgs, pkgs-unstable,inputs, ...}:
 
 {
+
+boot.extraModulePackages = with config.boot.kernelPackages; [
+    v4l2loopback
+  ];
+  boot.kernelModules = [ "v4l2loopback" ];
+  boot.extraModprobeConfig = ''
+    options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+  '';
+  security.polkit.enable = true;
+
 services.flatpak.enable = true;
   services.flatpak.packages = [
    "org.upscayl.Upscayl"
