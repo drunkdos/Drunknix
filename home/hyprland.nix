63b7795901d65b11c47653faa425b3269fec172e
drunkdos <drunkdos.97@gmail.com>
2025-10-26 00:20:33 +0200

    makoremove

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -3,24 +3,8 @@
 
  imports = [
    ./app/rofi.nix
-   ./app/fuzzel.nix
   ];
 
-#  services.mako = {
-#   enable = false;
-#   settings = {
-#     actions = true;
-#     anchor = "top-right";
-#     border-size= 2;
-#     border-radius = 8;
-#     default-timeout = 10;
-#     icons = true;
-#     ignore-timeout = false;
-#     layer = "top";
-#     margin = 8;
-#     markup = true;
-#     };
-#  };
 
 programs.wlogout = {
   enable = true;
