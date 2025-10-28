270ce8d5a8407627b533cd51adfc767d2977c7e7
drunkdos <drunkdos.97@gmail.com>
2025-10-28 02:05:29 +0100

    this work

--- modules/services/caddy.nix
+++ modules/services/caddy.nix
@@ -1,23 +1,31 @@
 { config, lib, pkgs, pkgs-unstable, ... }:
 
 {
-
   services.caddy = {
     enable = true;
-    virtualHosts."miodominio.noip.me".extraConfig = ''
-      # Jellyfin
-      handle /jellyfin/* {
-        reverse_proxy localhost:8096
-      }
+    virtualHosts."drunk.ddns.net" = {
+      # Configurazione del reverse proxy
+      extraConfig = ''
+        # Configurazione di base
+        bind 0.0.0.0
 
-      # Copypaste
-      handle /copypaste/* {
-        reverse_proxy localhost:3923
-      }
-    '';
+        # Route per Jellyfin
+        redir /jellyfin /jellyfin/
+          reverse_proxy /jellyfin/* localhost:8096
+        }
+
+        # Route per Copyparty
+        handle /copyparty/* {
+          reverse_proxy localhost:3923
+        }
+
+        # Pagina di default con messaggio "ciao"
+        handle {
+          respond "DIOCANE" 200
+        }
+      '';
+    };
   };
 
-
   networking.firewall.allowedTCPPorts = [ 80 443 ];
-
 }
