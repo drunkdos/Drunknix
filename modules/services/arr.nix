f0126016c94c33f5293ab6705be9dbda7812a6d8
drunkdos <drunkdos.97@gmail.com>
2025-11-16 00:20:29 +0100

    aaaarrrrrr

---
+++ modules/services/arr.nix
@@ -0,0 +1,19 @@
+{ config, pkgs, ... }:
+{
+
+services.prowlarr = {
+  enable = true;
+  openFirewall = true;
+};
+
+services.sonarr = {
+  enable = true;
+  openFirewall = true;
+};
+
+services.radarr = {
+  enable = true;
+  openFirewall = true;
+};
+
+}
