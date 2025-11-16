6ff2c6812bb8cdaec36a88720921120ae57d1f7d
drunkdos <drunkdos.97@gmail.com>
2025-11-16 22:26:28 +0100

    manca solo jellyseer, test migrazione torrent

--- modules/services/caddy.nix
+++ modules/services/caddy.nix
@@ -25,6 +25,50 @@
             # Proxy verso la porta di jellyfin
             reverse_proxy localhost:8096
           }
+
+          route /sonarr* {
+            reverse_proxy localhost:8989
+          }
+          route /radarr* {
+            reverse_proxy localhost:7878
+          }
+          route /prowlarr* {
+            reverse_proxy localhost:9696
+          }
+
+          # 1. Reindirizzamento senza slash finale (/torrent -> /torrent/)
+          route /torrent {
+            redir /torrent/ 308
+          }
+
+          # 2. Usa handle_path per RISCRIVERE il percorso (tolto /torrent)
+          handle_path /torrent/* {
+            reverse_proxy localhost:8080 {
+              # 3. Impostazioni Cruciali degli Header
+
+              # Rimuove Referer/Origin per aggirare problemi CSRF
+              header_up -Referer
+              header_up -Origin
+
+              # Correzione del percorso del cookie (fondamentale per il login su subpath)
+              copy_headers -set-cookie
+              @setcookie header Set-Cookie
+              rewrite @setcookie "SID=(.+); Path=/" "SID=$1; Path=/torrent/"
+            }
+          }
+        '';
+      };
+
+    # 2. Configurazione Sottodominio Jellyseerr
+      "jellyseer.drunk.ddns.net" = {
+        extraConfig = ''
+          # Configurazione Caddy più semplice possibile per sottodominio
+          reverse_proxy localhost:5055
+        '';
+      };
+    "torrent.drunk.ddns.net" = {
+        extraConfig = ''
+          reverse_proxy localhost:8080
         '';
       };
     };
