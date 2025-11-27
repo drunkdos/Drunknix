69a63d50f54b13a17905b2f6e3059cce280b17af
drunkdos <drunkdos.97@gmail.com>
2025-11-28 00:41:25 +0100

    first packaged app

---
+++ modules/animesaturn.nix
@@ -0,0 +1,44 @@
+{ config, lib, pkgs, ... }:
+
+let
+  # --- Definizione del Pacchetto ---
+  pname = "animesaturn-downloader";
+  version = "0.2.2"; # Controlla sempre l'ultima versione
+
+  animesaturn-pkg = pkgs.buildGoModule {
+    inherit pname version;
+
+    src = pkgs.fetchFromGitHub {
+      owner = "MrRainbow0704";
+      repo = "animesaturnDownloaderGo";
+      rev = "v${version}";
+      # Hash provvisorio per i sorgenti (Nix ti darà quello vero al primo errore)
+      hash = lib.fakeSha256;
+    };
+
+    # Hash provvisorio per le dipendenze Go
+    vendorHash = lib.fakeSha256;
+
+    doCheck = false;
+
+    meta = with lib; {
+      description = "Un modo semplice per scaricare anime in italiano (CLI)";
+      homepage = "https://github.com/MrRainbow0704/animesaturnDownloaderGo";
+      license = licenses.gpl3;
+      mainProgram = "animesaturn-downloader";
+    };
+  };
+
+  cfg = config.programs.animesaturn-downloader;
+
+in {
+  # --- Opzioni del Modulo ---
+  options.programs.animesaturn-downloader = {
+    enable = lib.mkEnableOption "Abilita animesaturn-downloader CLI";
+  };
+
+  # --- Configurazione (Cosa succede se enable = true) ---
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = [ animesaturn-pkg ];
+  };
+}
