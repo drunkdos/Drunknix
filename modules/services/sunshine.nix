94e1eed885ee7126ea84093984f2d299aa54dc31
drunkdos <drunkdos.97@gmail.com>
2025-12-11 00:35:13 +0100

    sunshine

---
+++ modules/services/sunshine.nix
@@ -0,0 +1,11 @@
+{ config, lib, pkgs, pkgs-unstable, ... }:
+{
+
+  services.sunshine = {
+    enable = true;
+    autoStart = true;
+    capSysAdmin = true; # only needed for Wayland -- omit this when using with Xorg
+    openFirewall = true;
+  };
+
+}
