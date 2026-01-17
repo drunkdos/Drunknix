ac96ea9e11fc2a2e31109a5941b7005fc8c8b54c
drunkdos <drunkdos.97@gmail.com>
2026-01-17 17:04:22 +0100

    add pufferpanel

---
+++ modules/services/gameserver.nix
@@ -0,0 +1,16 @@
+{ config, lib, pkgs, pkgs-unstable, ... }:
+{
+    services.pufferpanel = {
+        enable = true;
+        extraGroups = [drunkserver];
+        environment = {
+            PUFFER_WEB_HOST = ":8090";
+            PUFFER_DAEMON_SFTP_HOST = ":5657";
+            PUFFER_DAEMON_CONSOLE_BUFFER = "1000";
+            PUFFER_DAEMON_CONSOLE_FORWARD = "true";
+        };
+    };
+     networking.firewall = {
+       allowedTCPPorts = [ 8090 ];
+       };
+}
