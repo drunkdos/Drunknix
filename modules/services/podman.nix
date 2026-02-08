c3ad5a7fad1093ddd66acf9093aad88cb1c7495c
drunkdos <drunkdos.97@gmail.com>
2026-02-08 20:19:32 +0100

    Podman

---
+++ modules/services/podman.nix
@@ -0,0 +1,18 @@
+{ config, lib, pkgs, ...}:
+
+{
+virtualisation = {
+  containers.enable = true;
+  podman = {
+    enable = true;
+    dockerCompat = true;
+    defaultNetwork.settings.dns_enabled = true; # Required for containers under podman-compose to be able to talk to each other.
+  };
+};
+
+users.users.drunk = { # replace `<USERNAME>` with the actual username
+  extraGroups = [
+    "podman"
+  ];
+ };
+}
