44443e96a4626cf35c1b96aa5f1a9ad2fcd64f9f
drunkdos <drunkdos.97@gmail.com>
2026-03-13 00:10:44 +0100

    podman working so far

--- modules/services/podman.nix
+++ modules/services/podman.nix
@@ -1,18 +1,34 @@
-{ config, lib, pkgs, ...}:
+{
+  config,
+  lib,
+  pkgs,
+  pkgs-unstable,
+  ...
+}:
 
 {
-virtualisation = {
-  containers.enable = true;
-  podman = {
-    enable = true;
-    dockerCompat = true;
-    defaultNetwork.settings.dns_enabled = true; # Required for containers under podman-compose to be able to talk to each other.
+  virtualisation = {
+    containers.enable = true;
+    podman = {
+      enable = true;
+      dockerCompat = true;
+      defaultNetwork.settings.dns_enabled = true; # Required for containers under podman-compose to be able to talk to each other.
+    };
   };
-};
 
-users.users.drunk = { # replace `<USERNAME>` with the actual username
-  extraGroups = [
-    "podman"
-  ];
- };
+  users.users.drunk = {
+    # replace `<USERNAME>` with the actual username
+    extraGroups = [
+      "podman"
+    ];
+  };
+
+  environment.systemPackages =
+    (with pkgs; [
+      podman-desktop
+      podman-compose
+    ])
+    ++ (with pkgs-unstable; [
+      #unstable packages
+    ]);
 }
